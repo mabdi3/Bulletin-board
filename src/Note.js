242ad847385d1b5c103bc37eef5e1639243486e1
mabdi3 <mabdi3@gatech.edu>
2018-09-24 17:53:27 -0400

    randomized notes on startup from api

--- src/Note.js
+++ src/Note.js
@@ -15,7 +15,21 @@ class Note extends Component {
         this.renderForm = this.renderForm.bind(this)
         this.renderDisplay = this.renderDisplay.bind(this)
         this.save = this.save.bind(this);
+        this.randomBetween = this.randomBetween.bind(this)
     }
+
+    componentWillMount() {
+        this.style = {
+            right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween(0, window.inneHeight - 150, 'px'),
+            transform: `rotate{${this.randomBetween(-25, 25, 'degrees')}}`
+        }
+    }
+
+    randomBetween(x, y, s) {
+        return x + Math.ceil(Math.random() * (y - x)) + s
+    }
+
 
     edit() {
         this.setState({
@@ -38,7 +52,7 @@ class Note extends Component {
 
     renderForm() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <form onSubmit={this.save}>
                     <textarea ref={input => this._newText = input}/>
                     <button id="save"><FaSave /></button>
@@ -50,7 +64,7 @@ class Note extends Component {
 
     renderDisplay() {
         return (
-            <div className="note">
+            <div className="note" style={this.style}>
                 <p>{this.props.children}</p>
                 <span>
                     <button onClick={this.remove} id="remove"><FaTrashAlt /></button>
